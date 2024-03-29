a0a4475e764c38195a844cfdce02099ecd34cc17
IGDS801-20001472 <156256610+IGDS801-20001472@users.noreply.github.com>
2024-03-29 15:07:15 -0600

    Adicion del modulo Produccion con modelo de datos

--- models.py
+++ models.py
@@ -23,3 +23,34 @@ class Caja(db.Model):
     __tablename__ = 'caja'
     id = db.Column(db.Integer, primary_key = True)
     dinero = db.Column(db.Double)
+
+
+# Modelo de datos Produccion
+class Produccion(db.Model):
+    __tablename__ = 'produccion'
+    idProduccion = db.Column(db.Integer, primary_key = True)
+    idProducto = db.Column(db.Integer)
+    fechaProduccion = db.Column(db.Date)
+    descripcion = db.Column(db.String(100))
+    costoProduccion = db.Column(db.Double)
+    fechaCaducidad = db.Column(db.Date)
+    cantidadStock = db.Column(db.Integer)
+    cantidadProducida = db.Column(db.Integer)
+    estatusProduccion = db.Column(db.String(40))
+
+# Modelo de datos de producto
+class Producto(db.Model):
+    __tablename__ = 'producto'
+    idProducto = db.Column(db.Integer, primary_key = True)
+    idReceta = db.Column(db.Integer)
+    idMateriaPrima = db.Column(db.Integer)
+    nombreProducto = db.Column(db.String(64))
+    
+
+# Modelo de datos de Materia Prima (al que le toco esta tabla no se que mas vaya a agregar)
+# class Materia_Prima(db.Model):
+#     __tablename__ = 'materia_prima'
+#     idMateriaPrima = db.Column(db.Integer, primary_key = True)
+#     idProveedor = db.Column(db.Integer)
+#     nombreMateriaP = db.Column(db.String(64))
+#     cantidad = db.Column(db.Double)
