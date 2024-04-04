1d45aeb8439717813e3aca1967e20cd13582bddd
IGDS801-20001472 <156256610+IGDS801-20001472@users.noreply.github.com>
2024-04-03 19:11:41 -0600

    Avance de modulo produccion(funcion agregar)

--- templates/produccionModule/produccionController.py
+++ templates/produccionModule/produccionController.py
@@ -1,10 +1,29 @@
 from flask import render_template, request, redirect, url_for
 from models import db
+
 import forms
 from models import Produccion
 
 def produccion():
 
-    print('Si jala jaja.')
-
     return render_template('produccionModule/produccion.html')
+
+def guardarProduccion():
+
+    create_form = forms.produccionForm(request.form)
+
+    if request.method == 'POST':
+        costoProd = create_form.costoProduccion.data
+        fechaProd = create_form.fechaProduccion.data
+        fechaCad = create_form.fechaCaducidad.data
+
+        prod = Produccion(
+            costoProduccion = costoProd,
+            fechaProduccion = fechaProd,
+            fechaCaducidad = fechaCad
+        )
+        db.session.add(prod)
+        db.session.commit()
+
+
+    return 
